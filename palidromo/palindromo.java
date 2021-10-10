963a964d9f9af7abc398eac9fdeddcf6bacde815
Victória Alves <victoriaaalvessantos@gmail.com>
2021-10-10 15:51:00 -0300

    subindo algoritmo palindromo

---
+++ palidromo/palindromo.java
@@ -0,0 +1,29 @@
+package teste.palidromo;
+
+import java.util.Scanner;
+
+public class palindromo {
+
+	public static void main(String[] args) {
+
+		Scanner ler = new Scanner(System.in);
+		String palavra;
+		String palindromo = "";
+
+		System.out.println("Digite uma palavra: ");
+		palavra = ler.nextLine();
+
+		for (int i = palavra.length() - 1; i >= 0; i--) {
+			palindromo = palindromo + palavra.charAt(i);
+		}
+
+		if (palindromo.equals(palavra)) {
+			System.out.print(palindromo + " é um palíndromo!");
+
+		} else {
+			System.out.print(palavra + " não é um palíndromo!");
+		}
+
+	}
+
+}
